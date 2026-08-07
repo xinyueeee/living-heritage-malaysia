3e3ab409f6ed525228f2e998147fb90d82cff583
xinyueeee <xytan-wp23@student.tarc.edu.my>
2026-08-07 18:08:49 +0800

    Refactor according architecture design

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\ExperienceRepositoryInterface;
+use App\Repositories\Eloquent\EloquentExperienceRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            ExperienceRepositoryInterface::class,
+            EloquentExperienceRepository::class
+        );
     }
 
     /**
